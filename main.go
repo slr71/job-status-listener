81cbbce7c077890424828825b7faebbeb925b736
Ian McEwen <mian@cyverse.org>
2018-04-02 12:30:01 -0700

    Add initial version with just 'running' endpoint that expects everything in the body

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	_ "expvar"
 	"flag"
+	"net"
 	"net/http"
 
 	"github.com/cyverse-de/configurate"
@@ -10,6 +12,8 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+
+	"github.com/cyverse-de/messaging"
 )
 
 var log = logrus.WithFields(logrus.Fields{
@@ -21,7 +25,74 @@ var log = logrus.WithFields(logrus.Fields{
 var (
 	cfgPath = flag.String("config", "", "Path to the configuration file.")
 	cfg     *viper.Viper
+
+	client *messaging.Client
 )
+
+// JobUpdatePublisher is the interface for types that need to publish a job
+// update.
+type JobUpdatePublisher interface {
+	PublishJobUpdate(m *messaging.UpdateMessage) error
+}
+
+func running(client JobUpdatePublisher, job *messaging.JobDetails, hostname string, msg string) (*messaging.UpdateMessage, error) {
+	parsedIP := net.ParseIP(hostname)
+	if parsedIP == nil {
+		_, err := net.LookupIP(hostname)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	updateMessage := &messaging.UpdateMessage{
+		Job:     *job,
+		State:   messaging.RunningState,
+		Message: msg,
+		Sender:  hostname,
+	}
+
+	err := client.PublishJobUpdate(updateMessage)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	log.Info(msg)
+	return updateMessage, nil
+}
+
+type MessagePost struct {
+	Hostname string
+	Message  string
+	Job      *messaging.JobDetails
+}
+
+func postRunning(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	out := json.NewEncoder(w)
+
+	var updateMessage MessagePost
+
+	err := json.NewDecoder(r.Body).Decode(&updateMessage)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Error(err)
+		out.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	msg, err := running(client, updateMessage.Job, updateMessage.Hostname, updateMessage.Message)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Error(err)
+		out.Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+	out.Encode(msg)
+}
 
 func init() {
 	flag.Parse()
@@ -39,6 +110,7 @@ func loadConfig(cfgPath string) {
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.Handle("/debug/vars", http.DefaultServeMux)
+	r.Path("/running").Methods("POST").HandlerFunc(postRunning)
 
 	return r
 }
@@ -46,6 +118,17 @@ func newRouter() *mux.Router {
 func main() {
 	log.Info("Starting up the job-status-listener service.")
 	loadConfig(*cfgPath)
+
+	uri := cfg.GetString("amqp.uri")
+	exchange := cfg.GetString("amqp.exchange.name")
+	var err error
+	client, err = messaging.NewClient(uri, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	client.SetupPublishing(exchange)
 
 	r := newRouter()
 
